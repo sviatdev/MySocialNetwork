15c9d170e855e8fa84c6a1dadc322a22c7b4f2df
sviatdev <sviatiq@gmail.com>
2021-10-11 19:52:09 +0300

    #2 Changed thymeleaf to mustache extension. Added main method.

--- src/main/java/com/sviatdev/mysocialnetwork/GreetingsController.java
+++ src/main/java/com/sviatdev/mysocialnetwork/GreetingsController.java
@@ -5,13 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class GreetingsController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World")
-                                       String name, Model model){
-        model.addAttribute("name", name);
+                                       String name, Map<String, Object> map){
+        map.put("name", name);
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> map){
+        map.put("text", "Hello, sviatdev!");
+        return "main";
     }
 }
